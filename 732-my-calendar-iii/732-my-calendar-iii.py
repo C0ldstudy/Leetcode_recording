3d39cced2abbef0dddc010bd5540276f19e3dccb
c0ldstudy <xjc04@qq.com>
2022-10-07 13:39:46 -0700

    Time: 3697 ms (16.06%), Space: 14.5 MB (62.35%) - LeetHub

---
+++ 732-my-calendar-iii/732-my-calendar-iii.py
@@ -0,0 +1,41 @@
+
+class MyCalendarThree:
+
+    def __init__(self):
+        self.data = {}
+
+    def book(self, start: int, end: int) -> int:
+        if not start in self.data.keys():
+            self.data[start] = 0
+        if not end in self.data.keys():
+            self.data[end] = 0     
+        self.data[start] +=  1
+        self.data[end] +=  -1
+        self.data = dict(sorted(self.data.items()))
+        result = 0
+        current = 0
+        for i in self.data.keys():
+            current += self.data[i]
+            if current > result:
+                result = current
+        return result
+
+        
+        
+        
+        
+        
+        
+#         if end > len(self.data):
+#             for i in range(len(self.data), end, 1):
+#                 self.data.append(0)
+#         for i in range(start, end):
+#             self.data[i] += 1
+#         return max(self.data)
+        
+        
+
+
+# Your MyCalendarThree object will be instantiated and called as such:
+# obj = MyCalendarThree()
+# param_1 = obj.book(start,end)
