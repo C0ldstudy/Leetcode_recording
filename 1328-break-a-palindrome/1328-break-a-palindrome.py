98d23d4fecc46d47d47136d6e1d9b3bd71e252b1
c0ldstudy <xjc04@qq.com>
2022-10-10 12:44:22 -0700

    Time: 65 ms (13.05%), Space: 14 MB (13.13%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -0,0 +1,23 @@
+class Solution:
+    def breakPalindrome(self, palindrome: str) -> str:
+        if (len(palindrome) < 2):
+            return ''
+        number = []
+        for i in palindrome:
+            if i != 'a':
+                number.append(ord(i))
+        # special case: 'aa'
+        if len(number) < 2 :
+            palindrome = palindrome[:-1] + 'b'
+            return palindrome
+
+        target = chr(number[0])
+        # print(target)
+        for i, cha in enumerate(palindrome):
+            if target == cha:
+                # palindrome[i] = 'a'
+                palindrome = palindrome[:i] + 'a' + palindrome[(i+1):]
+
+                # print(palindrome)
+                return palindrome
+        return ''
