08d41adf0aa0f592c1beca0664cc4f0d3e0778bf
c0ldstudy <xjc04@qq.com>
2022-10-15 21:22:28 -0700

    Time: 4624 ms (11.93%), Space: 14.1 MB (81.63%) - LeetHub

---
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
+        if len(jobDifficulty) < d:
+            return -1
+        elif len(jobDifficulty) == d:
+            return sum(jobDifficulty)
+        dp = [[float('inf')]*len(jobDifficulty) for i in range(d)] # d *jobs
+        dp[0][0] = jobDifficulty[0]
+        for i in range(1, len(jobDifficulty)):
+            if i < d:
+                dp[i][i] = sum(jobDifficulty[:i+1])
+            dp[0][i] = max(dp[0][i-1], jobDifficulty[i])
+
+        for j in range(1, d):
+            for i in range(j+1, len(jobDifficulty)):
+                for k in range(i):
+                    # print(k, i)
+                    local_min = dp[j-1][k] + max(jobDifficulty[k+1:i+1])
+                    # print(dp[j-1][k], jobDifficulty[k+1:i+1])
+                    dp[j][i] = min(dp[j][i], local_min)
+                    # dp[j][i] = local_min
+        # print(dp)
+        return dp[d-1][len(jobDifficulty)-1]
+                
+        
+
+        
+            
+        
+        
