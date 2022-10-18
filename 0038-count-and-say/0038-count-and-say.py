387e586fd60111516eb6b1a73fe7bfe61ef303c1
c0ldstudy <xjc04@qq.com>
2022-10-17 17:36:32 -0700

    Time: 114 ms (17.57%), Space: 13.9 MB (54.87%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,28 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        result = []
+        result.append('1')
+        for i in range(1, n):
+            result.append(self.say(result[i-1]))
+        print(result)
+        return result[n-1]
+   
+    def say(self, num):
+        temp = ''
+        cur = 0
+        count = 1
+        while cur < len(num):
+            if cur == len(num)-1:
+                temp += str(count)+str(num[cur])
+                break
+            if num[cur] == num[cur+1]:
+                count += 1
+                cur += 1
+            else:
+                temp += str(count)+str(num[cur])
+                count = 1
+                cur += 1
+        # temp = 
+        return str(temp)
+            
+            
