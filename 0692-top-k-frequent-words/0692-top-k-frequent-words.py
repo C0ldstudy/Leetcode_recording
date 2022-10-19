ffce53277469899ee10eb548f9bc8992cc4d419b
c0ldstudy <xjc04@qq.com>
2022-10-18 17:58:52 -0700

    Time: 46 ms (99.85%), Space: 14 MB (27.71%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,14 +1,14 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        # use a dictionary to save the words and the frequencies        
         word_dict = {}
         for word in words:
             if not word in word_dict.keys():
                 word_dict[word] = 1
             else:
                 word_dict[word] += 1
-        print(word_dict)
+        # reverse to find the words that have the same frequency                
         result_keys = sorted(word_dict.items(), key=lambda x: x[1], reverse=True)
-        # result_keys = result_keys[:k]
         result = []
         frequency_dict = {}
         for i in result_keys:
@@ -16,15 +16,11 @@ class Solution:
                 frequency_dict[i[1]] = [i[0]]
             else:
                 frequency_dict[i[1]].append(i[0])
-        print(frequency_dict)
+        # output the top k
         for i in sorted(frequency_dict.keys(), reverse=True):
             for j in sorted(frequency_dict[i]):
                 result.append(j)
                 if len(result) == k:
                     return result
-            
-            
-        # result = [i[0] for i in result_keys]
-        # print(result_keys)
         
         return result
