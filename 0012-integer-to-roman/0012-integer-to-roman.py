0969a342df76ec6bec270d202aa381c333dd5644
c0ldstudy <xjc04@qq.com>
2022-10-19 19:58:23 -0700

    Time: 97 ms (48.67%), Space: 13.9 MB (80.12%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,38 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        number_list = [1000, None, 100, None, 10, None, 1]
+        symbol_list = ['M', 'D', 'C', 'L', 'X', 'V', 'I']
+        result = ''
+        for i, number in enumerate(number_list):
+            if number == None:
+                continue
+            integer = int(num/number)
+            # print(num, result, number)
+            
+            if integer == 0:
+                continue
+            elif integer == 4:
+                result += symbol_list[i] + symbol_list[i-1]
+                num = num - integer * number
+                # num
+                continue
+            elif integer == 9:
+                result += symbol_list[i] + symbol_list[i-2]
+                num = num - integer * number
+                continue   
+            elif integer == 5:
+                result += symbol_list[i-1]
+                num = num - integer * number
+                continue                 
+            elif integer >5:
+                result += symbol_list[i-1] + symbol_list[i]*(integer-5)
+                num = num - integer * number                
+                continue
+            result += symbol_list[i] * integer
+            num = num - integer * number
+        return result
+            
+            
+            
+            
+        
