758810cf65df6fff03803a5107291e342577bebe
c0ldstudy <xjc04@qq.com>
2024-09-24 21:24:10 -0400

    Time: 709 ms (90.96%), Space: 27.3 MB (98.61%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.py
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.py
@@ -1,24 +1,15 @@
 class Solution:
-    def longestCommonPrefix(self, arr1, arr2):
-        arr1_prefixes = set()  # Set to store all prefixes from arr1
-
-        # Step 1: Build all possible prefixes from arr1
-        for val in arr1:
-            while val not in arr1_prefixes and val > 0:
-                # Insert current value as a prefix
-                arr1_prefixes.add(val)
-                # Generate the next shorter prefix by removing the last digit
-                val //= 10
-
-        longest_prefix = 0
-
-        # Step 2: Check each number in arr2 for the longest matching prefix
-        for val in arr2:
-            while val not in arr1_prefixes and val > 0:
-                # Reduce val by removing the last digit if not found in the prefix set
-                val //= 10
-            if val > 0:
-                # Length of the matched prefix using log10 to determine the number of digits
-                longest_prefix = max(longest_prefix, len(str(val)))
-
-        return longest_prefix
+    def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
+        prefix = set()
+        for i in range(len(arr1)):
+            while not (arr1[i] in prefix) and arr1[i] > 0:
+                prefix.add(arr1[i])
+                arr1[i] //= 10
+        longest = 0
+        for i in range(len(arr2)):
+            while not (arr2[i] in prefix) and arr2[i] > 0:
+                arr2[i] //= 10
+            if arr2[i] > 0:
+                longest = max(longest, len(str(arr2[i])))
+        return longest
+    
