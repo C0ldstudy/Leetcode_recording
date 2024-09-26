e0ed35ceaf2a4f478d9438ed96ef339b37c9ce5b
c0ldstudy <xjc04@qq.com>
2024-09-26 10:03:07 -0400

    Time: 29 ms (93.25%), Space: 16.7 MB (33.56%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,35 +1,16 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        if numRows == 0: return []
         if numRows == 1: return [[1]]
-        # if numRows == 2: return [,1]
+        if numRows == 2: return [[1], [1,1]]
+        res = [1] * numRows
+                
         
         previous_rows = self.generate(numRows-1)
-        previous_row = previous_rows[-1]
-        result = [1]
+        pre_row = previous_rows[-1]
+        
         for i in range(1, numRows-1):
-            result.append(previous_row[i-1]+previous_row[i])
-        result.append(1)
-        previous_rows.append(result)
+            res[i] = (pre_row[i-1] + pre_row[i])
+            
+        previous_rows.append(res)
         return previous_rows
-    
-    
-    
-# class Solution:
-#     def generate(self, numRows: int) -> List[List[int]]:
-#         if numRows == 0:
-#             return []
-#         if numRows == 1:
-#             return [[1]]
-
-#         prev_rows = self.generate(numRows - 1)
-#         prev_row = prev_rows[-1]
-#         current_row = [1]
-
-#         for i in range(1, numRows - 1):
-#             current_row.append(prev_row[i - 1] + prev_row[i])
-
-#         current_row.append(1)
-#         prev_rows.append(current_row)
-
-#         return prev_rows
+            
