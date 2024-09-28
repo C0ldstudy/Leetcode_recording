9028bf067484b4c39d044e766f2087054bf83f8a
c0ldstudy <xjc04@qq.com>
2024-09-28 15:22:09 -0400

    Time: 5655 ms (18.17%), Space: 16.8 MB (5.45%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,21 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        m, n = len(board), len(board[0])
+        if len(word) > m*n: return False
+        # cnt = Counter(word)
+        # length = Counter(chain(*board))
+        # # print(cnt, length)
+        # if not cnt <= length:      # [b] there are not enough
+        #     return False  
+                
+        def dfs(i,j, s):
+            if s == len(word): return True
+            if (0<=i < m) and (0<= j < n) and board[i][j] == word[s]:
+                board[i][j] = '#'
+                adj = [(i,j+1), (i,j-1), (i-1,j), (i+1,j)]
+                dp = any(dfs(ii,jj,s+1) for ii, jj in adj)
+                board[i][j] = word[s]
+                return dp
+            return False
+        return any(dfs(i,j,0) for i,j in product(range(m),range(n)))
+                
