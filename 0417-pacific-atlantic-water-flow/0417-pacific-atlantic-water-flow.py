61df857e2709efdba59248fb4e4d82b0e1f7884b
c0ldstudy <xjc04@qq.com>
2024-09-30 10:29:09 -0400

    Time: 228 ms (26.96%), Space: 18.1 MB (62.88%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,35 +1,33 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        if not heights or not heights[0]:
-            return []
-        m, n = len(heights), len(heights[0])
+        rows, cols = len(heights), len(heights[0])
+        pac_queue = deque()
+        atl_queue = deque()
         
-        pacific_queue = deque()
-        atlantic_queue = deque()
-        for i in range(m):
-            pacific_queue.append((i, 0))
-            atlantic_queue.append((i, n-1))
-        for i in range(n):
-            pacific_queue.append((0, i))
-            atlantic_queue.append((m-1, i))
+        for i in range(rows):
+            pac_queue.append((i,0))
+            atl_queue.append((i, cols-1))
+        for j in range(cols):
+            pac_queue.append((0, j))
+            atl_queue.append((rows-1, j))
+            
         def bfs(queue):
             reachable = set()
             while queue:
-                (row, col) = queue.popleft()
-                reachable.add((row, col))
-                for (x,y) in [(1,0), (0,1), (-1,0), (0,-1)]:
-                    new_row, new_col = row +x, col+y
-                    if new_row < 0 or new_row >= m or new_col < 0 or new_col >= n:
+                (row,col) = queue.popleft()
+                reachable.add((row,col))
+                adj = [(row+1, col), (row-1,col),(row,col+1),(row,col-1)]
+                for new_row,new_col in adj:
+                    if new_row<0 or new_row >=rows or new_col<0 or new_col >=cols:
                         continue
-                    if (new_row,new_col) in reachable:
+                    elif (new_row,new_col) in reachable:
                         continue
-                    if heights[new_row][new_col]< heights[row][col]:
+                    elif heights[new_row][new_col] < heights[row][col]:
                         continue
-                    queue.append((new_row, new_col))
+                    queue.append((new_row,new_col))
             return reachable
         
-        pracific_reachable = bfs(pacific_queue)
-        atlantic_reachable = bfs(atlantic_queue)
-        
-        return list(pracific_reachable.intersection(atlantic_reachable))
-                        
+        pac_reachable = bfs(pac_queue)
+        atl_reachable = bfs(atl_queue)
+        return pac_reachable.intersection(atl_reachable)
+            
