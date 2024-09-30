9a3ce48201ac92b1f96790bfc16f6fa9f34e3e26
c0ldstudy <xjc04@qq.com>
2024-09-30 15:42:34 -0400

    Time: 907 ms (70.30%), Space: 64.3 MB (8.56%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.py
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.py
@@ -1,18 +1,14 @@
 class Solution:
     def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
         envelopes.sort(key=lambda x: (x[0], -x[1]))
-        sorted_env = [i[1] for i in envelopes]
+        env = [e[1] for e in envelopes]
         
         sub = []
-        for ei in range(len(sorted_env)):
-            idx = bisect_left(sub, sorted_env[ei])
+        for ei in range(len(env)):
+            idx = bisect_left(sub, env[ei])
             if idx == len(sub):
-                sub.append(sorted_env[ei])
+                sub.append(env[ei])
             else:
-                sub[idx] = sorted_env[ei]
+                sub[idx] = env[ei]
         return len(sub)
-                        
-                
-        
-        
         
