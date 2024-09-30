a484cf663a7dacdc42cdc04010dc2354d7471172
c0ldstudy <xjc04@qq.com>
2024-09-29 23:07:38 -0400

    Time: 5709 ms (17.51%), Space: 16.7 MB (32.70%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,21 +1,23 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        m, n = len(board), len(board[0])
-        if len(word) > m*n: return False
-        cnt = Counter(word)
+        rows, cols = len(board), len(board[0])
+        if rows * cols < len(word): return False
+        counter = Counter(word)
         length = Counter(chain(*board))
-        # print(cnt, length)
-        if not cnt <= length:      # [b] there are not enough
-            return False  
+        if counter > length: return False
+        
+        
+        def dfs(i, j, s):
+            if s==len(word): return True
+            if (0 <= i < rows) and (0<=j<cols) and (word[s] == board[i][j]):
+                tmp = board[i][j]
+                board[i][j] = "#"
+                adj = [(i-1, j), (i+1, j), (i,j+1), (i,j-1)]
                 
-        def dfs(i,j, s):
-            if s == len(word): return True
-            if (0<=i < m) and (0<= j < n) and board[i][j] == word[s]:
-                board[i][j] = '#'
-                adj = [(i,j+1), (i,j-1), (i-1,j), (i+1,j)]
-                dp = any(dfs(ii,jj,s+1) for ii, jj in adj)
-                board[i][j] = word[s]
+                dp = any(dfs(ii, jj, s+1) for ii, jj in adj)
+                board[i][j] = tmp
                 return dp
             return False
-        return any(dfs(i,j,0) for i,j in product(range(m),range(n)))
+        return any(dfs(i,j,0) for i,j in product(range(rows), range(cols)))
+        
                 
