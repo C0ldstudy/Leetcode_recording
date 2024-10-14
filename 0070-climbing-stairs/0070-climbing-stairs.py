2cf766939ef6b474ace8a1ab54fe126cc3ce277f
c0ldstudy <xjc04@qq.com>
2024-10-14 17:17:52 -0400

    Time: 32 ms (73.50%), Space: 16.4 MB (67.53%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,16 +1,12 @@
 class Solution:
-    @cache
     def climbStairs(self, n: int) -> int:
-        temp = [0] * n
-        res = self.check(n-1, n, temp)
-        return res
-    
-    def check(self, i, n , temp):
-        if i == 0: return 1
-        elif i == 1: return 2
-        if temp[i] > 0:
-            return temp[i]
-        else:
-            temp[i] = self.check(i-1, n, temp) + self.check(i-2, n, temp)
-        return temp[i]
-        
+        if n == 1: return 1
+        elif n == 2: return 2
+        elif n == 3: return 3
+        dp = [0] * (n+1)
+        dp[2] = 2
+        dp[3] = 3
+        for i in range(4, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
+            
