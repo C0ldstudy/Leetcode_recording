a697a7f2c51fa0af687f549605f1a2548afdb0a8
c0ldstudy <xjc04@qq.com>
2024-10-13 22:20:41 -0400

    Time: 138 ms (91.70%), Space: 16.9 MB (93.40%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        color = {}
+        for node in range(len(graph)):
+            if node not in color:
+                stack = [node]
+                color[node] = 0
+                while stack:
+                    node = stack.pop()
+                    for nei in graph[node]:
+                        if nei not in color:
+                            stack.append(nei)
+                            color[nei] = ~color[node]
+                        elif color[nei] == color[node]:
+                            return False
+        return True
+                            
+                            
+        
+                
+            
