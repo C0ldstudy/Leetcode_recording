a0a0cd91f367a5d895d013f74acef33ba7f4c69a
c0ldstudy <xjc04@qq.com>
2024-10-14 22:47:35 -0400

    Time: 57 ms (96.34%), Space: 16.6 MB (77.71%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,19 +1,18 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        rows, cols = len(image),len(image[0])
-        original_color = image[sr][sc]
         if image[sr][sc] == color: return image
+        rows = len(image)
+        cols = len(image[0])
+        ori_color = image[sr][sc]
         image[sr][sc] = color
-        def dfs(i,j,image):
-            if (0<= i < rows) and (0<= j < cols):
-                
-                adj = [(i+1, j), (i-1, j), (i, j+1), (i,j-1)]
-                for ii, jj in adj:
-                    if (0<= ii < rows) and (0<= jj < cols):
-                        if image[ii][jj] == original_color:
-                            image[ii][jj] = color
-                            # res.append([ii,jj])
-                            dfs(ii, jj, image)
-        dfs(sr, sc, image)
-
+        
+        stack = [(sr,sc)]
+        while stack:
+            row, col = stack.pop()
+            adj = [(row+1, col), (row-1, col), (row,col+1), (row, col-1)]
+            for i, j in adj:
+                if (0<=i<rows) and (0<=j<cols):
+                    if (image[i][j] == ori_color) :
+                        image[i][j] = color
+                        stack.append((i,j))
         return image
