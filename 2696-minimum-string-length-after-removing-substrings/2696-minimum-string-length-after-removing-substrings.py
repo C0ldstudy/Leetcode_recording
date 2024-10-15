07c9daab43677da1bc7842f56bcec048a28f82bf
c0ldstudy <xjc04@qq.com>
2024-10-14 23:18:05 -0400

    Time: 41 ms (73.62%), Space: 16.6 MB (57.88%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.py
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.py
@@ -1,8 +1,7 @@
 class Solution:
     def minLength(self, s: str) -> int:
-        while ('AB' in s) or ('CD' in s):
-            if 'AB' in s:
-                s = s.replace("AB", "")
-            if 'CD' in s:
-                s = s.replace('CD', "")
+        while 'AB' in s or 'CD' in s:
+            s = s.replace("AB", "")
+            s = s.replace("CD", "")
         return len(s)
+            
