19e38aa5d6bbc6a3aff22dd713b9e9e54385cf95
c0ldstudy <xjc04@qq.com>
2024-10-14 20:23:28 -0400

    Time: 259 ms (27.17%), Space: 33.2 MB (92.93%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -1,12 +1,18 @@
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
-        num_to_rank = {}
-        sorted_arr = sorted(arr)
+        if len(arr) == 0: return []
+        dic = {}
         rank = 1
-        for i in range(len(sorted_arr)):
-            if i > 0 and sorted_arr[i] > sorted_arr[i - 1]:
+        s_arr = sorted(arr)
+        dic[s_arr[0]] = 1
+        for i in range(1, len(arr)):            
+            if s_arr[i] > s_arr[i-1]:
                 rank += 1
-            num_to_rank[sorted_arr[i]] = rank
+                dic[s_arr[i]] = rank
         for i in range(len(arr)):
-            arr[i] = num_to_rank[arr[i]]
+            arr[i] = dic[arr[i]]
         return arr
+            
+
+            
+        
