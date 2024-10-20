310ee57773e0bbcca57978216857f74bedea14ae
c0ldstudy <xjc04@qq.com>
2024-10-20 18:56:05 -0400

    Time: 52 ms (99.65%), Space: 35.8 MB (26.89%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -3,16 +3,13 @@ class Solution:
         if len(arr) == 0: return []
         dic = {}
         rank = 1
-        s_arr = sorted(arr)
-        dic[s_arr[0]] = 1
-        for i in range(1, len(arr)):            
-            if s_arr[i] > s_arr[i-1]:
+        for n in sorted(arr):
+            if n not in dic:
+                dic[n] = rank
                 rank += 1
-                dic[s_arr[i]] = rank
-        for i in range(len(arr)):
-            arr[i] = dic[arr[i]]
-        return arr
+        # print(dic)
+        result = []
+        for n in arr:
+            result.append(dic[n])
+        return result
             
-
-            
-        
