a8a63bf42dcc9e35614a253a45bd99f1c1b8587f
c0ldstudy <xjc04@qq.com>
2024-10-20 09:41:33 -0400

    Time: 223 ms (64.78%), Space: 16.6 MB (68.27%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
@@ -3,24 +3,11 @@ class Solution:
         max_or_value = 0
         for num in nums:
             max_or_value |= num
-        return self._count_subsets(nums, 0, 0, max_or_value)
-
-    def _count_subsets(
-        self, nums: List[int], index: int, current_or: int, target_or: int
-    ) -> int:
-        # Base case: reached the end of the array
+        return self.count(nums, 0, 0, max_or_value)
+    
+    def count(self, nums, index, current_or, target_or):
         if index == len(nums):
             return 1 if current_or == target_or else 0
-
-        # Don't include the current number
-        count_without = self._count_subsets(
-            nums, index + 1, current_or, target_or
-        )
-
-        # Include the current number
-        count_with = self._count_subsets(
-            nums, index + 1, current_or | nums[index], target_or
-        )
-
-        # Return the sum of both cases
-        return count_without + count_with
+        count_without = self.count(nums, index+1, current_or, target_or)
+        count_with = self.count(nums, index+1, current_or | nums[index], target_or)
+        return count_without+count_with
