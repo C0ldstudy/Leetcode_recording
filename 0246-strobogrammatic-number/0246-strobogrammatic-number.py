8e7349313d064d2eaad7754b60bea52a3feb1bec
c0ldstudy <xjc04@qq.com>
2024-10-20 14:05:59 -0400

    Time: 0 ms (100.00%), Space: 16.5 MB (37.94%) - LeetHub

--- 0246-strobogrammatic-number/0246-strobogrammatic-number.py
+++ 0246-strobogrammatic-number/0246-strobogrammatic-number.py
@@ -1,12 +1,6 @@
 class Solution:
     def isStrobogrammatic(self, num: str) -> bool:
         num = list(num)
-        # if len(num) == 1:
-        #     if num[0] in ['1','8', '0']:
-        #         return True
-        #     else:
-        #         return False
-            
         if ('3' in num) or ('4' in num) or  ('7' in num) or ('2' in num) or ('5' in num):
             return False
         length = len(num) // 2
