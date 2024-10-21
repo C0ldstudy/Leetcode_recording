1e6acfb2de660b152dd9fddcf578b2c0d0ad1c1b
c0ldstudy <xjc04@qq.com>
2024-10-20 21:27:15 -0400

    Time: 0 ms (100.00%), Space: 16.6 MB (24.17%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -1,21 +1,11 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        if len(str1) < len(str2): self.gcdOfStrings(str2, str1)
-        
-        def check(st, st_d):
-            time = len(st)//len(st_d)
-            if st == st_d * time: return True
-            return False
-        
-        l = len(str2)
-        while l > 0:
-            if check(str1, str2[:l]) and check(str2, str2[:l]): 
-                return str2[:l]
+        if len(str1) < len(str2): return self.gcdOfStrings(str2, str1)
+        result = str2
+        while result:
+            if (str1 == result* (len(str1)//len(result))) and (str2 == result* (len(str2)//len(result))):
+                return result
             else:
-                # if l % 2 != 0: return "" 
-                l -= 1
-            # print(l)
-        return ""
-
+                result = result[:-1]
+        return result
         
-    
