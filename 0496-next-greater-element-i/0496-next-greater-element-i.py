20325571d6ccdf382a51266e82333a7f3505782f
c0ldstudy <xjc04@qq.com>
2024-10-23 21:18:39 -0400

    Time: 50 ms (45.32%), Space: 16.7 MB (79.52%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,13 +1,12 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        result = [-1] *len(nums1)
-        for idx, n1 in enumerate(nums1):
+        result = [-1] * len(nums1)
+        for idx1 in range(len(nums1)):
             found = False
             for n2 in nums2:
-                if n2 == n1: found = True
-                if (found == True) and (n2 > n1):
-                    result[idx] = n2
+                if n2 == nums1[idx1]:
+                    found = True
+                elif found and n2 > nums1[idx1]:
+                    result[idx1] = n2
                     break
         return result
-                    
-                    
