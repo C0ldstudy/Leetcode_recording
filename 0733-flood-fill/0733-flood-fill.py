3498f982add64fb8c682cf3125a1b0c728b44cf3
c0ldstudy <xjc04@qq.com>
2024-11-06 22:26:24 -0800

    Time: 0 ms (100.00%), Space: 16.8 MB (39.05%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,18 +1,18 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         if image[sr][sc] == color: return image
-        rows = len(image)
-        cols = len(image[0])
+        rows, cols = len(image), len(image[0])
+        stack = [(sr, sc)]
         ori_color = image[sr][sc]
-        image[sr][sc] = color
         
-        stack = [(sr,sc)]
         while stack:
             row, col = stack.pop()
-            adj = [(row+1, col), (row-1, col), (row,col+1), (row, col-1)]
-            for i, j in adj:
+            adj = [(row, col),(row+1, col), (row-1, col), (row,col+1), (row, col-1)]
+            for i,j in adj:
                 if (0<=i<rows) and (0<=j<cols):
-                    if (image[i][j] == ori_color) :
+                    if image[i][j] == ori_color:
                         image[i][j] = color
                         stack.append((i,j))
         return image
+                    
+            
