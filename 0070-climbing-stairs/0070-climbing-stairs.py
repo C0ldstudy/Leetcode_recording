292ddde6d7992bcaa917fe778c616761d508168e
c0ldstudy <xjc04@qq.com>
2024-11-06 22:40:58 -0800

    Time: 0 ms (100.00%), Space: 16.8 MB (15.72%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,12 +1,8 @@
 class Solution:
+    @cache
     def climbStairs(self, n: int) -> int:
-        if n == 1: return 1
+        if n == 0: return 0
+        elif n == 1: return 1
         elif n == 2: return 2
-        elif n == 3: return 3
-        dp = [0] * (n+1)
-        dp[2] = 2
-        dp[3] = 3
-        for i in range(4, n+1):
-            dp[i] = dp[i-1] + dp[i-2]
-        return dp[n]
-            
+        result = self.climbStairs(n-1)+self.climbStairs(n-2)
+        return result
